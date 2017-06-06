80080e53c7ca4ef417231014998c702b198892b2
craig-mulligan <craig@resin.io>
2017-06-06 12:39:24 +0100

    Updates

---
+++ path---index-125e3a9531ba31b2093e.js
@@ -0,0 +1,2 @@
+webpackJsonp([5],{"./node_modules/json-loader/index.js!./.cache/json/index.json":function(e,t){e.exports={data:{site:{siteMetadata:{title:"Gatsby Starter Blog"}},allMarkdownRemark:{edges:[{node:{fields:{slug:"/2015-05-28-hi-folks/"},frontmatter:{title:"New Beginnings"}}},{node:{fields:{slug:"/2015-05-01-hello-world/"},frontmatter:{title:"Hello World"}}},{node:{fields:{slug:"/2015-05-06-my-second-post/"},frontmatter:{title:"My Second Post!"}}}]}},pathContext:{}}}});
+//# sourceMappingURL=path---index-125e3a9531ba31b2093e.js.map
